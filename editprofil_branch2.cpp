798890105774de76c2854901855ce81db2916277
cchoirun <niamchoirun020@gmail.com>
2022-12-30 22:56:39 +0700

    Memperbaiki error

--- editprofil_branch2.cpp
+++ editprofil_branch2.cpp
@@ -4,5 +4,5 @@
 
 int main(){
 
-    std:: << "Fingerprint yang dimasukkan dapat lebih dari 5 fingerprint" << std::endl;
+    std::cout << "Fingerprint yang dimasukkan dapat lebih dari 5 fingerprint" << std::endl;
 }
